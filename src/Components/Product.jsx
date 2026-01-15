ad84f2fdf01d2a2bc3bf3ec626e9080fcf8f8762
vedika82 <anandvedika420@gmail.com>
2026-01-16 01:01:33 +0530

    bahut galtiyan hai

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -5,8 +5,9 @@ import './Product.css'
 const Product = ()=>
 {return (
     <>
+    <div className="gh">
+
     <div className="top-bar">
-       
        {/* <div> */}
         <nav className="links">
             <a href="">Conference Expense Planner</a>
@@ -16,12 +17,66 @@ const Product = ()=>
             <a href='/Product.jsx' className='btn-link'>
             <button className="show-details-button">ShowDetails</button></a>
         </nav>
-        {/* </div> */}
-    </div>
-    <div className="snd-bar">
-        <p>Venue Room Selection</p>
+        {/* </div> */} 
     </div>
 
+    <div className="snd-bar">
+      Venue Room Selection
+    </div>
+ 
+ <div>
+    <div lassName="image">
+        <img  className="image1" src="https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_1280.jpg"/>
+        <p>Conference Room</p>
+        <p>(Capacity:15)</p>
+        <p>$1500</p>
+        <div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+        <div className="image">
+        <img  className="image1" src="https://cdn.pixabay.com/photo/2016/08/16/09/53/international-conference-1597531_1280.jpg"/>
+           <p>Conference Room</p>
+        <p>(Capacity:15)</p>
+        <p>$1500</p>
+        <div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+        <div className="image">
+        <img  className="image1" src="https://cdn.pixabay.com/photo/2015/01/08/18/11/laptops-593296_1280.jpg"/>
+           <p>Conference Room</p>
+        <p>(Capacity:15)</p>
+        <p>$1500</p>
+        <div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+        <div className="image">
+        <img className="image1" src="https://cdn.pixabay.com/photo/2019/01/02/07/30/convention-center-3908238_1280.jpg"/>
+           <p>Conference Room</p>
+        <p>(Capacity:15)</p>
+        <p>$1500</p>
+        <div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+        <div className="image">
+        <img className="image1" src="https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_1280.jpg"/>
+           <p>Conference Room</p>
+        <p>(Capacity:15)</p>
+        <p>$1500</p>
+        <div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+ </div>
+</div>
     </>
 
 )
